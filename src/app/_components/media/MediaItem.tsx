b08187c676c9cf0df4491a65fc91c635b8f24d7f
Tim Koprivnik <tim.koprivnik@agiledrop.com>
2023-08-21 10:30:28 +0200

    Fix show img placeholder if error

--- src/app/_components/media/MediaItem.tsx
+++ src/app/_components/media/MediaItem.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FC, SyntheticEvent } from 'react';
+import { FC, SyntheticEvent, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './MediaItem.module.scss';
@@ -21,30 +21,35 @@ const MediaItem: FC<MediaItemProps> = ({
   mediaType = '',
   mediaProperty = '',
   releaseDateProperty = '',
-}) => (
-  <Card className={styles['media-item']}>
-    <Link href={`/${mediaType}/${media.id}`}>
-      <Image
-        src={`https://image.tmdb.org/t/p/w200${media.poster_path}`}
-        alt={media[mediaProperty]?.toString() || ''}
-        width={200}
-        height={280}
-        onError={(e: SyntheticEvent<HTMLImageElement>) => {
-          e.currentTarget.src =
-            '/assets/images/placeholder-media-details-image.png';
-        }}
-      />
-    </Link>
-    <div className={styles.content}>
-      <UserScore score={media.vote_average} small />
+}) => {
+  const [imgSrc, setImgSrc] = useState(
+    `https://image.tmdb.org/t/p/w200${media.poster_path}`
+  );
+
+  return (
+    <Card className={styles['media-item']}>
       <Link href={`/${mediaType}/${media.id}`}>
-        <h2 className={styles.title}>{media[mediaProperty]?.toString()}</h2>
+        <Image
+          src={imgSrc}
+          alt={media[mediaProperty]?.toString() || ''}
+          width={200}
+          height={280}
+          onError={(e: SyntheticEvent<HTMLImageElement>) => {
+            setImgSrc('/assets/images/placeholder-media-details-image.png');
+          }}
+        />
       </Link>
-      <p className={styles['release-date']}>
-        {formatDate(media[releaseDateProperty]?.toString())}
-      </p>
-    </div>
-  </Card>
-);
+      <div className={styles.content}>
+        <UserScore score={media.vote_average} small />
+        <Link href={`/${mediaType}/${media.id}`}>
+          <h2 className={styles.title}>{media[mediaProperty]?.toString()}</h2>
+        </Link>
+        <p className={styles['release-date']}>
+          {formatDate(media[releaseDateProperty]?.toString())}
+        </p>
+      </div>
+    </Card>
+  );
+};
 
 export default MediaItem;
