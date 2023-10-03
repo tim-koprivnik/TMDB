519e93dfeb374631365ce1649c09e8b897bb70a4
Tim Koprivnik <tim.koprivnik@agiledrop.com>
2023-10-03 14:21:50 +0200

    Update providers

---
+++ src/app/_store/providers.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'react-redux';
+import store from './store';
+
+const queryClient = new QueryClient();
+
+export default function ReduxProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>{children}</Provider>
+    </QueryClientProvider>
+  );
+}
