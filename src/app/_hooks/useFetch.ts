c39f86ac15e03388d4c04a2b195ed918aa271de2
Tim Koprivnik <tim.koprivnik@agiledrop.com>
2023-10-03 14:22:16 +0200

    Prevent duplicate API calls with tanstack/react-query

--- src/app/_hooks/useFetch.ts
+++ src/app/_hooks/useFetch.ts
@@ -1,6 +1,4 @@
-'use client';
-
-import { useState, useEffect, useRef } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 interface FetchData<T> {
   data: T | null;
@@ -8,52 +6,23 @@ interface FetchData<T> {
   error: Error | null;
 }
 
+const fetcher = async (url: string) => {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error('Fetch failed');
+  return response.json();
+};
+
 const useFetch = <T>(url: string): FetchData<T> => {
-  const [data, setData] = useState<T | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<Error | null>(null);
+  const { data, error, isLoading } = useQuery<T, Error>(
+    ['fetchData', url],
+    () => fetcher(url)
+  );
 
-  const abortController = useRef(new AbortController());
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      setError(null);
-
-      try {
-        abortController.current.abort();
-        abortController.current = new AbortController();
-        const res = await fetch(url, {
-          signal: abortController.current.signal,
-        });
-
-        if (!res.ok) {
-          throw new Error('Fetch failed');
-        }
-
-        const resData: T = await res.json();
-        setData(resData);
-        setLoading(false);
-      } catch (err: unknown) {
-        if (err instanceof Error) {
-          if (err.name === 'AbortError') {
-            console.error('Fetch aborted');
-          } else {
-            setError(err);
-          }
-        }
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-
-    return () => {
-      abortController.current.abort();
-    };
-  }, [url]);
-
-  return { data, loading, error };
+  return {
+    data: data || null,
+    loading: isLoading,
+    error,
+  };
 };
 
 export default useFetch;
