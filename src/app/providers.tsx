7575a584434bed90a359c60d349db5a80ee443ae
Tim Koprivnik <tim.koprivnik@agiledrop.com>
2023-10-16 11:21:37 +0200

    Move providers file

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,7 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './_store/store';
 
 const queryClient = new QueryClient();
 
